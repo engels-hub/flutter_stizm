956364f418daaf035cf3721136459f583c721e08
engels-hub <artjompapsh2003@gmail.com>
2021-01-25 23:31:20 +0200

    StIzm 2.1.2

---
+++ lib/service/style.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const LargeTextSize = 26.0;
+const MediumTextSize = 20.0;
+const BodyTextSize = 16.0;
+
+const String FontNameDefault ="Maven Pro";
+const String BodyFontDefault ="Poppins";
+
+const AppBarStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w400,
+  fontSize: LargeTextSize,
+  color: Colors.white,
+);
+
+const TitleTextStyle = TextStyle(
+  fontFamily: BodyFontDefault,
+  fontWeight: FontWeight.w400,
+  fontSize: MediumTextSize,
+  color: Colors.black54,
+);
+const BodyTextStyle = TextStyle(
+  fontFamily: BodyFontDefault,
+  fontWeight: FontWeight.w400,
+  fontSize: BodyTextSize,
+  color: Colors.black,
+);
